5c3987c4c550b36724549c0070580c5f5e84ca9d
ziggs <cayliseseveneleven@gmail.com>
2026-09-12 21:52:40 -0400

    Add bug tracker status tags to guildConfig schema
    Added bug tracker status tags to guildConfig schema.

--- Schemas.js/guildConfig.js
+++ Schemas.js/guildConfig.js
@@ -7,6 +7,11 @@ const guildConfigSchema = new mongoose.Schema({
   bugForumChannelId: { type: String, default: null }, // forum channel to watch for bug threads
   bugStaffRoleId:    { type: String, default: null },  // role allowed to manage bugs
   bugPingRoleId:     { type: String, default: null },  // role to ping in thread when a new bug is reported
+
+  // Bug tracker Discord forum status tags
+  bugReceivedTagId:   { type: String, default: null },
+  bugInProgressTagId: { type: String, default: null },
+  bugResolvedTagId:   { type: String, default: null },
 });
 
 module.exports = mongoose.model("GuildConfig", guildConfigSchema);
